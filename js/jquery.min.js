2c27aad8d1f4df0a0262e1f6e889ccbf9381b9c6
tsuchiyama <tsuchiyama007@gmail.com>
2016-09-21 16:46:15 +0000

    first commit

---
+++ js/jquery.min.js
@@ -0,0 +1,10 @@
+<script>
+  $(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:false,
+        items:1,
+        autoplay:true
+    })
+  });
+</script>
